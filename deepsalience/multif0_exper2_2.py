08142924e5dc06f141d4b86c4f725865cf1dccce
Rachel Bittner <rachel.bittner@nyu.edu>
2017-04-15 19:47:33 -0400

    imports again horray

--- deepsalience/multif0_exper2_2.py
+++ deepsalience/multif0_exper2_2.py
@@ -3,6 +3,7 @@ import keras
 from keras.models import Model
 from keras.layers import Dense, Input, Reshape, Lambda
 from keras.layers.convolutional import Conv2D
+from keras import backend as K
 import os
 
 import experiment
