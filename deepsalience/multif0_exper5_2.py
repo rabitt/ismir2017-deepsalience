0f0bdbbc4f79fe7084e419c950fdbea8f1f9e5d4
Rachel Bittner <rachel.bittner@nyu.edu>
2017-04-15 19:45:39 -0400

    imports whoops

--- deepsalience/multif0_exper5_2.py
+++ deepsalience/multif0_exper5_2.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import keras
 from keras.models import Model
+from keras.layers import Dense, Input, Reshape, Lambda
+from keras.layers.convolutional import Conv2D
 import os
 
 import experiment
